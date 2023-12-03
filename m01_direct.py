c8ebe21f468cde4a3dad45d2181683cb205f4ab6
Forrest Trepte <forresttrepte+github@hotmail.com>
2023-12-02 17:16:18 -0800

    rules update

--- m01_direct.py
+++ m01_direct.py
@@ -7,7 +7,11 @@ You are playing the game So Clover. Create clues for the following word pair:
 Output your clue CLUE in the following format:
 Best: CLUE
 
-Your clue should be a single word. Use lowercase unless it is a proper noun or capitalization the word would help the guesser understand the clue.
+Your clue should be a single word.
+It cannot be a made up word. It should be in a dictionary or be in common use in pop culture.
+For example, brand names, names of people, acronyms, numbers, and onomatopoeia are OK.
+Your word cannot be in the same family or contain the words from the word pair.
+Use lowercase unless it is a proper noun or if capitalizing the word would help the guesser understand the clue.
 """
 
 
