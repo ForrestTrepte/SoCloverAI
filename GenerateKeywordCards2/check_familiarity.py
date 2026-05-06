918718544809f380573decc79406ef54c5a9a894
Forrest Trepte <forresttrepte+github@hotmail.com>
2026-05-06 17:48:32 +0000

    log cost

--- GenerateKeywordCards2/check_familiarity.py
+++ GenerateKeywordCards2/check_familiarity.py
@@ -1,12 +1,14 @@
 from litellm import completion
+from litellm.cost_calculator import completion_cost
 
 from get_prompt import get_prompt
 
 
-def check_familiarity(model: str) -> str:
+def check_familiarity(model: str) -> tuple[str, float]:
     prompt = get_prompt("check_familiarity", "check_familiarity", {})
     response = completion(
         model=model,
         messages=[{"role": "user", "content": prompt}],
     )
-    return response.choices[0].message.content
+    cost = completion_cost(response)
+    return response.choices[0].message.content, cost
