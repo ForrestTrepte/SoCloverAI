141bde087703462d160f6da67cb9990beddf6ca7
Forrest Trepte <forresttrepte+github@hotmail.com>
2026-05-06 17:40:49 +0000

    gemini small model

--- GenerateKeywordCards2/models.py
+++ GenerateKeywordCards2/models.py
@@ -6,6 +6,6 @@ except ImportError:
 small_models = [
     "openai/gpt-5.4-mini-2026-03-17",
     "anthropic/claude-haiku-4-5-20251001",
-    # "gemini/gemini-3-flash-preview",
-    "gemini/gemini-3.1-flash-lite-preview",
+    # "gemini/gemini-3.1-flash-lite-preview",
+    "gemini/gemini-3-flash-preview",
 ]
